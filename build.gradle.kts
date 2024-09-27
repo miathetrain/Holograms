762cd025e4ea6de140855c9dcae330c483bb1e1f
Mia <miathewiccancatgirl@gmail.com>
2024-09-27 11:14:35 -0400

    publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
-apply(plugin = "maven-publish")
+
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "2.0.20-Beta2"
     id("org.jetbrains.kotlin.kapt") version "2.0.20-Beta2"
     id("io.github.goooler.shadow") version "8.1.8"
+    id("maven-publish")
+    id("java-library")
 }
 
-group "com.cybergirly.hub"
+group "net.holograms"
 version "1.0-SNAPSHOT"
 
 repositories {
@@ -20,4 +22,16 @@ dependencies {
     compileOnly("cn.nukkit:nukkit:1.0-SNAPSHOT")
     compileOnly("com.creeperface.nukkit.placeholderapi:PlaceholderAPI:1.4-SNAPSHOT")
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "net.holograms"
+            artifactId = "holograms"
+            version = "1.0"
+
+            from(components["java"])
+        }
+    }
 }
